899e996016fc56653f60918210e9b07f558d7674
Paul Burt <paul.burt@gmail.com>
2015-02-22 22:42:45 +0000

    Updated to latest version of markdown-clj

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    {:dependencies [[org.clojure/clojurescript "0.0-2913"]
                    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                    [org.clojure/core.incubator "0.1.3"]
-                   [markdown-clj "0.9.62"]
+                   [markdown-clj "0.9.63"]
                    [rum "0.2.5"]
                    [sablono "0.3.4"]
                    [com.cognitect/transit-cljs "0.8.205"]]
